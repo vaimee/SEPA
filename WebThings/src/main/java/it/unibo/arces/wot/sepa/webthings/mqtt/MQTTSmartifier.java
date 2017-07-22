c6ef84ee59498d5de39e7f733befcc5a7d8a5358
Luca Roffia <lroffia@gmail.com>
2017-07-22 16:36:01 +0200

    chore: Working on MQTT monitoring application

--- WebThings/src/main/java/it/unibo/arces/wot/sepa/webthings/mqtt/MQTTSmartifier.java
+++ WebThings/src/main/java/it/unibo/arces/wot/sepa/webthings/mqtt/MQTTSmartifier.java
@@ -1,5 +1,23 @@
 package it.unibo.arces.wot.sepa.webthings.mqtt;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.InvalidKeyException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.util.NoSuchElementException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallback;
 import org.eclipse.paho.client.mqttv3.MqttClient;
@@ -12,74 +30,28 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-import it.unibo.arces.wot.framework.ThingDescription;
-import it.unibo.arces.wot.framework.interaction.EventPublisher;
 import it.unibo.arces.wot.sepa.commons.protocol.SSLSecurityManager;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-
-import java.net.URISyntaxException;
-
-import java.security.InvalidKeyException;
-import java.security.KeyManagementException;
-
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-
-import java.security.UnrecoverableKeyException;
-
-import java.security.cert.CertificateException;
-
-import java.util.HashMap;
-import java.util.NoSuchElementException;
-
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-
-public class MQTTWebThing implements MqttCallback {
+public class MQTTSmartifier implements MqttCallback {
 	private MqttClient mqttClient;
 
-	private static final Logger logger = LogManager.getLogger("MQTTAdapter");
+	private static final Logger logger = LogManager.getLogger("MQTTSmartifier");
 
 	private boolean created = false;
-
-	public static HashMap<String, String> debugHash = new HashMap<String, String>();
-	private HashMap<String, String> topicResponseCache = new HashMap<String, String>();
 
 	private String serverURI = null;
 	private String[] topicsFilter = { "#" };
 	private boolean sslEnabled = false;
-	private String clientID = "MQTTWebThing";
-
-	// WoT APIs
-	private EventPublisher event = null;
-	private ThingDescription webThing = null;
-	private String mqttEvent = "wot:mqttMessageReceived";
+	private String clientID = "MQTTSmartifier";
 
 	private SSLSecurityManager sm = new SSLSecurityManager("TLSv1","sepa.jks","sepa2017","sepa2017");
 	
-	public MQTTWebThing(String jsonFile) throws UnrecoverableKeyException, KeyManagementException, InvalidKeyException,
+	public MQTTSmartifier(String jsonFile) throws UnrecoverableKeyException, KeyManagementException, InvalidKeyException,
 			IllegalArgumentException, KeyStoreException, NoSuchAlgorithmException, CertificateException,
 			FileNotFoundException, NoSuchElementException, NullPointerException, ClassCastException,
 			NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, IOException, URISyntaxException {
 
 		loadJSON(jsonFile);
-
-		String thingURI = new String(serverURI);
-		thingURI = thingURI.replace(":", "_");
-		thingURI = thingURI.replace("/", "_");
-		thingURI = "wot:MQTTBroker_" + thingURI;
-		webThing = new ThingDescription(thingURI, clientID);
-		webThing.addEvent(mqttEvent, "MQTT Event");
-		event = new EventPublisher(thingURI);
 	}
 
 	private void loadJSON(String fileName) throws IOException {
@@ -89,10 +61,13 @@ public class MQTTWebThing implements MqttCallback {
 
 		if (in != null) {
 			JsonObject root = new JsonParser().parse(in).getAsJsonObject();
+			
+			//MQTT
+			JsonObject mqtt =  root.get("mqtt").getAsJsonObject();
 
-			String url = root.get("url").getAsString();
-			int port = root.get("port").getAsInt();
-			JsonArray topics = root.get("topics").getAsJsonArray();
+			String url = mqtt.get("url").getAsString();
+			int port = mqtt.get("port").getAsInt();
+			JsonArray topics = mqtt.get("topics").getAsJsonArray();
 
 			topicsFilter = new String[topics.size()];
 			int i = 0;
@@ -101,7 +76,7 @@ public class MQTTWebThing implements MqttCallback {
 				i++;
 			}
 
-			if (root.get("ssl") != null) this.sslEnabled = root.get("ssl").getAsBoolean();
+			if (mqtt.get("ssl") != null) this.sslEnabled = mqtt.get("ssl").getAsBoolean();
 			else this.sslEnabled = false;
 			
 			if (sslEnabled){
@@ -109,6 +84,9 @@ public class MQTTWebThing implements MqttCallback {
 			} else {
 				serverURI = "tcp://" + url + ":" + String.format("%d", port);
 			}
+			
+			//Semantic mappings
+			
 		}
 		if (in != null)
 			in.close();
@@ -128,17 +106,7 @@ public class MQTTWebThing implements MqttCallback {
 	public void messageArrived(String topic, MqttMessage value) throws Exception {
 		logger.debug(topic + " " + value.toString());
 
-		event.post(mqttEvent, topic + "&" + value.toString());
-
-		topicResponseCache.put(topic, topic + "&" + value.toString());
-
-		if (debugHash.containsKey(topic)) {
-			if (!debugHash.get(topic).equals(value.toString())) {
-				logger.debug(topic + " " + debugHash.get(topic) + "-->" + value.toString());
-			}
-		}
-
-		debugHash.put(topic, value.toString());
+	
 	}
 
 	public static void main(String[] args) throws IOException, URISyntaxException, UnrecoverableKeyException,
@@ -147,7 +115,7 @@ public class MQTTWebThing implements MqttCallback {
 			ClassCastException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
 
 		if (args.length != 1) {
-			logger.error("Please specify the configuration file (.json)");
+			logger.error("Please specify the JSAP file (.jsap)");
 			System.exit(1);
 		}
 
