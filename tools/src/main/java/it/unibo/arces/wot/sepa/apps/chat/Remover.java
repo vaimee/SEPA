ba828338d7a8aa8b8c6e418ba1bfdb6de9cf4613
Luca Roffia <lroffia@gmail.com>
2017-12-21 14:00:59 +0100

    Chat app updated

---
+++ tools/src/main/java/it/unibo/arces/wot/sepa/apps/chat/Remover.java
@@ -0,0 +1,84 @@
+package it.unibo.arces.wot.sepa.apps.chat;
+
+import it.unibo.arces.wot.sepa.commons.exceptions.SEPAPropertiesException;
+import it.unibo.arces.wot.sepa.commons.exceptions.SEPAProtocolException;
+import it.unibo.arces.wot.sepa.commons.response.ErrorResponse;
+import it.unibo.arces.wot.sepa.commons.response.Response;
+import it.unibo.arces.wot.sepa.commons.response.SubscribeResponse;
+import it.unibo.arces.wot.sepa.commons.sparql.ARBindingsResults;
+import it.unibo.arces.wot.sepa.commons.sparql.Bindings;
+import it.unibo.arces.wot.sepa.commons.sparql.BindingsResults;
+import it.unibo.arces.wot.sepa.commons.sparql.RDFTermURI;
+import it.unibo.arces.wot.sepa.pattern.Aggregator;
+import it.unibo.arces.wot.sepa.pattern.ApplicationProfile;
+
+public class Remover extends Aggregator {
+	private Bindings sender = new Bindings();
+	private boolean joined = false;
+	
+	public Remover(String senderURI) throws SEPAProtocolException, SEPAPropertiesException {
+		super(new ApplicationProfile("chat.jsap"), "RECEIVED", "REMOVE");
+		sender.addBinding("sender", new RDFTermURI(senderURI));
+	}
+
+	public boolean joinChat() {
+		if (joined)
+			return true;
+
+		Response ret = subscribe(sender);
+		joined = !ret.isError();
+				
+		if (joined) onAddedResults(((SubscribeResponse) ret).getBindingsResults());
+
+		return joined;
+	}
+
+	public boolean leaveChat() {
+		if(!joined) return true;
+		
+		Response ret = unsubscribe();	
+		joined = !ret.isUnsubscribeResponse();
+		
+		return !joined;
+	}
+	
+	@Override
+	public void onResults(ARBindingsResults results) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onAddedResults(BindingsResults results) {
+		for (Bindings bindings : results.getBindings()) {
+			System.out.println("Remove "+bindings);
+			update(bindings);
+		}
+		
+	}
+
+	@Override
+	public void onRemovedResults(BindingsResults results) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onPing() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onBrokenSocket() {
+		joined = false;
+		
+	}
+
+	@Override
+	public void onError(ErrorResponse errorResponse) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ tools/src/main/java/it/unibo/arces/wot/sepa/apps/chat/Users.java
@@ -0,0 +1,93 @@
+package it.unibo.arces.wot.sepa.apps.chat;
+
+import java.util.HashMap;
+import java.util.Set;
+
+import it.unibo.arces.wot.sepa.commons.exceptions.SEPAPropertiesException;
+import it.unibo.arces.wot.sepa.commons.exceptions.SEPAProtocolException;
+import it.unibo.arces.wot.sepa.commons.response.ErrorResponse;
+import it.unibo.arces.wot.sepa.commons.response.Response;
+import it.unibo.arces.wot.sepa.commons.response.SubscribeResponse;
+import it.unibo.arces.wot.sepa.commons.sparql.ARBindingsResults;
+import it.unibo.arces.wot.sepa.commons.sparql.Bindings;
+import it.unibo.arces.wot.sepa.commons.sparql.BindingsResults;
+import it.unibo.arces.wot.sepa.pattern.ApplicationProfile;
+import it.unibo.arces.wot.sepa.pattern.Consumer;
+
+public class Users extends Consumer {
+	private HashMap<String,String> usersList = new HashMap<String,String>();
+	private boolean joined = false;
+	
+	public Users() throws SEPAProtocolException, SEPAPropertiesException {
+		super(new ApplicationProfile("chat.jsap"), "USERS");
+	}
+
+	public boolean joinChat() {
+		if (joined)
+			return true;
+
+		Response ret = subscribe(null);
+		joined = !ret.isError();
+				
+		if (joined) onAddedResults(((SubscribeResponse) ret).getBindingsResults());
+
+		return joined;
+	}
+
+	public boolean leaveChat() {
+		if(!joined) return true;
+		
+		Response ret = unsubscribe();	
+		joined = !ret.isUnsubscribeResponse();
+		
+		return !joined;
+	}
+	
+	public Set<String> getUsers() {
+		return usersList.keySet();
+	}
+	
+	public String getUserName(String user) {
+		return usersList.get(user);
+	}
+	
+	@Override
+	public void onResults(ARBindingsResults results) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onAddedResults(BindingsResults results) {
+		for (Bindings bindings : results.getBindings()) {
+			usersList.put(bindings.getBindingValue("user"), bindings.getBindingValue("userName"));
+		}
+		
+	}
+
+	@Override
+	public void onRemovedResults(BindingsResults results) {
+		for (Bindings bindings : results.getBindings()) {
+			usersList.remove(bindings.getBindingValue("user"));
+		}
+	}
+
+	@Override
+	public void onPing() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onBrokenSocket() {
+		joined = false;
+		
+	}
+
+	@Override
+	public void onError(ErrorResponse errorResponse) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
