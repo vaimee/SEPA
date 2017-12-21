3c2a78cd2cfeb94aaba85cb740dee8f7f17af4ca
Luca Roffia <lroffia@gmail.com>
2017-12-21 14:03:02 +0100

    Chat files updated

--- tools/src/main/java/it/unibo/arces/wot/sepa/apps/chat/Sender.java
+++ tools/src/main/java/it/unibo/arces/wot/sepa/apps/chat/Sender.java
@@ -3,113 +3,27 @@ package it.unibo.arces.wot.sepa.apps.chat;
 import it.unibo.arces.wot.sepa.commons.exceptions.SEPAPropertiesException;
 import it.unibo.arces.wot.sepa.commons.exceptions.SEPAProtocolException;
 import it.unibo.arces.wot.sepa.commons.exceptions.SEPASecurityException;
-import it.unibo.arces.wot.sepa.commons.response.ErrorResponse;
-import it.unibo.arces.wot.sepa.commons.response.Response;
-import it.unibo.arces.wot.sepa.commons.response.SubscribeResponse;
-import it.unibo.arces.wot.sepa.commons.sparql.ARBindingsResults;
 import it.unibo.arces.wot.sepa.commons.sparql.Bindings;
-import it.unibo.arces.wot.sepa.commons.sparql.BindingsResults;
 import it.unibo.arces.wot.sepa.commons.sparql.RDFTermLiteral;
-import it.unibo.arces.wot.sepa.pattern.Aggregator;
+import it.unibo.arces.wot.sepa.commons.sparql.RDFTermURI;
 import it.unibo.arces.wot.sepa.pattern.ApplicationProfile;
+import it.unibo.arces.wot.sepa.pattern.Producer;
 
-public class MessageSender extends Aggregator {
+public class Sender extends Producer {
 
 	private Bindings message = new Bindings();
-
-	private ChatListener listener;
-	private boolean joined = false;
 	
-	public MessageSender(String nickname,ApplicationProfile jsap,ChatListener listener)
+	public Sender(String senderURI)
 			throws SEPAProtocolException, SEPASecurityException, SEPAPropertiesException {
-		super(jsap, "SEND_MESSAGE","SEND_MESSAGE");
+		super(new ApplicationProfile("chat.jsap"), "SEND");
 		
-		message.addBinding("sender", new RDFTermLiteral(nickname));
-
-        this.listener = listener;
+		message.addBinding("sender", new RDFTermURI(senderURI));
 	}
 	
-	public boolean sendMessage(String receiver,String text) {
-		message.addBinding("receiver", new RDFTermLiteral(receiver));
+	public boolean sendMessage(String receiverURI,String text) {
+		message.addBinding("receiver", new RDFTermURI(receiverURI));
 		message.addBinding("text", new RDFTermLiteral(text));
 		
-		Response ret = update(message);
-		
-		return ret.isUpdateResponse();
+		return update(message).isUpdateResponse();
 	}
-
-	public boolean joinChat() {
-		if (joined)
-			return true;
-
-		Response ret = subscribe(message);
-		joined = !ret.isError();
-
-		if (joined) {
-			for (Bindings bindings : ((SubscribeResponse) ret).getBindingsResults().getBindings()) {
-				listener.onMessageSent(
-						new Message(
-								message.getBindingValue("sender"),
-								bindings.getBindingValue("receiver"),
-								bindings.getBindingValue("text"),
-								bindings.getBindingValue("time")));
-			}
-		}
-
-		return joined;
-	}
-
-	public boolean leaveChat() {
-		if(!joined) return true;
-		
-		Response ret = unsubscribe();
-		joined = !ret.isUnsubscribeResponse();
-		
-		return !joined;
-	}
-
-	@Override
-	public void onAddedResults(BindingsResults results){
-		for (Bindings bindings : results.getBindings()) {
-			listener.onMessageSent(
-					new Message(
-							message.getBindingValue("sender"),
-							bindings.getBindingValue("receiver"),
-							bindings.getBindingValue("text"),bindings.
-							getBindingValue("time")));
-		}
-	}
-
-	@Override
-	public void onResults(ARBindingsResults results) {
-		
-	}
-
-	@Override
-	public void onRemovedResults(BindingsResults results) {
-		for (Bindings bindings : results.getBindings()) {
-			listener.onMessageRead(
-					new Message(
-							message.getBindingValue("sender"),
-							bindings.getBindingValue("receiver"),
-							bindings.getBindingValue("text"),
-							bindings.getBindingValue("time")));
-		}
-	}
-
-	@Override
-	public void onPing() {
-
-	}
-
-	@Override
-	public void onBrokenSocket() {
-		joined = false;
-	}
-
-	@Override
-	public void onError(ErrorResponse errorResponse) {
-
-	}
-
 }
