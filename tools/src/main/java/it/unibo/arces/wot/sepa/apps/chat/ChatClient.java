fb49e8d82bf835254b7c804a8cab0cf92794308a
Luca Roffia <lroffia@gmail.com>
2017-12-21 14:00:59 +0100

    chore: Chat app updated

--- tools/src/main/java/it/unibo/arces/wot/sepa/apps/chat/ChatClient.java
+++ tools/src/main/java/it/unibo/arces/wot/sepa/apps/chat/ChatClient.java
@@ -3,37 +3,32 @@ package it.unibo.arces.wot.sepa.apps.chat;
 import it.unibo.arces.wot.sepa.commons.exceptions.SEPAPropertiesException;
 import it.unibo.arces.wot.sepa.commons.exceptions.SEPAProtocolException;
 import it.unibo.arces.wot.sepa.commons.exceptions.SEPASecurityException;
-import it.unibo.arces.wot.sepa.pattern.ApplicationProfile;
 
-public abstract class BasicChatClient implements ChatListener {
+public abstract class ChatClient implements ChatListener {
 
-	private MessageSender sender;
-	private MessageReceiver receiver;
-	private String nickname;
+	protected Sender sender;
+	private Receiver receiver;
+	private Remover remover;
 	
-	public BasicChatClient(String nickname,ApplicationProfile app) throws SEPAProtocolException, SEPASecurityException, SEPAPropertiesException {
-		sender = new MessageSender(nickname,app,this);
-		receiver = new MessageReceiver(nickname,app,this);
-		this.nickname = nickname;
+	public ChatClient(String userURI) throws SEPAProtocolException, SEPASecurityException, SEPAPropertiesException {
+		sender = new Sender(userURI);
+		receiver = new Receiver(userURI,this);
+		remover = new Remover(userURI);
 	}
 	
-	public String getNickname() {
-		return nickname;
-	}
-
 	public boolean joinChat() {
-		if (!sender.joinChat()) return false;
+		if (!remover.joinChat()) return false;
 		if (!receiver.joinChat()) return false;
 		return true;
 	}
 
 	public boolean leaveChat() {
-		if (!sender.leaveChat()) return false;
+		if (!remover.leaveChat()) return false;
 		if (!receiver.leaveChat()) return false;
 		return true;
 	}
 
-	public boolean sendMessage(String receiver,String message) {
-		return sender.sendMessage(receiver,message);
+	public boolean sendMessage(String receiverURI,String message) {
+		return sender.sendMessage(receiverURI,message);
 	}
 }
