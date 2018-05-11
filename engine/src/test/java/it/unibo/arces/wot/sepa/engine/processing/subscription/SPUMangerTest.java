4b1acf28ac6d3200cad0de80a66fa5262e953c98
reluc <relu.cri@gmail.com>
2018-05-11 09:52:12 +0200

    refactor: Refactor code style changes

--- engine/src/test/java/it/unibo/arces/wot/sepa/engine/processing/subscription/SPUMangerTest.java
+++ engine/src/test/java/it/unibo/arces/wot/sepa/engine/processing/subscription/SPUMangerTest.java
@@ -1,21 +1,13 @@
 package it.unibo.arces.wot.sepa.engine.processing.subscription;
 
-import it.unibo.arces.wot.sepa.commons.exceptions.SEPAProtocolException;
-import it.unibo.arces.wot.sepa.commons.protocol.SPARQL11Properties;
-import it.unibo.arces.wot.sepa.commons.request.SubscribeRequest;
 import it.unibo.arces.wot.sepa.commons.response.Response;
 import it.unibo.arces.wot.sepa.commons.response.UpdateResponse;
 import it.unibo.arces.wot.sepa.commons.sparql.BindingsResults;
-import it.unibo.arces.wot.sepa.engine.core.EventHandler;
-import it.unibo.arces.wot.sepa.engine.processing.SPU;
-import it.unibo.arces.wot.sepa.engine.processing.SPUSync;
 import it.unibo.arces.wot.sepa.engine.processing.subscriptions.ISubscriptionProcUnit;
 import it.unibo.arces.wot.sepa.engine.processing.subscriptions.SPUManager;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.util.concurrent.Semaphore;
 
 public class SPUMangerTest {
 
@@ -29,29 +21,29 @@ public class SPUMangerTest {
     @Test
     public void registerTest(){
         FakeSPU spu = new FakeSPU("Test");
-        spuManger.Register(spu);
+        spuManger.register(spu);
         Assert.assertEquals("SPU not registered",spuManger.size(),1);
     }
 
     @Test
     public void unRegisterTest(){
         FakeSPU spu = new FakeSPU("123Test");
-        spuManger.Register(spu);
+        spuManger.register(spu);
         Assert.assertEquals("SPU not registered",spuManger.size(),1);
-        spuManger.UnRegister("123Test");
+        spuManger.unRegister("123Test");
         Assert.assertEquals("SPU not succesfully deleted",spuManger.size(),0);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void unRegisterInvalidID(){
-        spuManger.UnRegister("pluto");
+        spuManger.unRegister("pluto");
     }
 
     @Test
     public void isValidUIDTest(){
-        Assert.assertFalse(spuManger.isValidSPUID("Cap"));
-        spuManger.Register(new FakeSPU("Ironman"));
-        Assert.assertTrue(spuManger.isValidSPUID("Ironman"));
+        Assert.assertFalse(spuManger.isValidSpuId("Cap"));
+        spuManger.register(new FakeSPU("Ironman"));
+        Assert.assertTrue(spuManger.isValidSpuId("Ironman"));
     }
 
     private class FakeSPU implements ISubscriptionProcUnit{
