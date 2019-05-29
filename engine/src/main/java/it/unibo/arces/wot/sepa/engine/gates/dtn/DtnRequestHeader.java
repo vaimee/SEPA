81466f5529613db48c848c12886d7ed89ba0d296
Bisa <andrea.bisacchi5@studio.unibo.it>
2019-05-29 16:59:08 +0200

    Inserted comments in DTN headers

--- engine/src/main/java/it/unibo/arces/wot/sepa/engine/gates/dtn/DtnRequestHeader.java
+++ engine/src/main/java/it/unibo/arces/wot/sepa/engine/gates/dtn/DtnRequestHeader.java
@@ -4,6 +4,13 @@ import java.nio.ByteBuffer;
 
 public class DtnRequestHeader {
 	
+	// XXX insert here the request header parameters
+	
+	/**
+	 * Creates a request header from the buffer inserted in a bundle
+	 * @param buffer the data inside the bundle
+	 * @return the request header created
+	 */
 	public static DtnRequestHeader getRequestHeader(ByteBuffer buffer) {
 		return new DtnRequestHeader();
 	}
--- engine/src/main/java/it/unibo/arces/wot/sepa/engine/gates/dtn/DtnResponseHeader.java
+++ engine/src/main/java/it/unibo/arces/wot/sepa/engine/gates/dtn/DtnResponseHeader.java
@@ -8,14 +8,26 @@ public class DtnResponseHeader {
 	
 	private BundleTimestamp timestamp;
 	
+	/**
+	 * Creates a reponse header
+	 * @param timestamp the timestamp of bundle (used to identify uniquely the bundle)
+	 */
 	public DtnResponseHeader(BundleTimestamp timestamp) {
 		this.timestamp = timestamp;
 	}
 	
+	/**
+	 * Returns the header size
+	 * @return the header size
+	 */
 	public int getHeaderSize() {
-		return Integer.BYTES * 2;
+		return Integer.SIZE / 8 * 2;
 	}
 	
+	/**
+	 * Inserts the header in a byte buffer
+	 * @param buffer the byte buffer
+	 */
 	public void insertHeaderInByteBuffer(ByteBuffer buffer) {
 		buffer.putInt(this.timestamp.getSeconds());
 		buffer.putInt(this.timestamp.getSequenceNumber());
