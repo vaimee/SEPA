a58417eefadd1b366a169b907a8c837ba8d7759c
Luca Roffia <lroffia@gmail.com>
2019-07-31 12:54:54 +0200

    refactor: Minor refactory

--- engine/src/main/java/it/unibo/arces/wot/sepa/engine/processing/UpdateProcessingThread.java
+++ engine/src/main/java/it/unibo/arces/wot/sepa/engine/processing/UpdateProcessingThread.java
@@ -9,22 +9,28 @@ import it.unibo.arces.wot.sepa.commons.response.Response;
 import it.unibo.arces.wot.sepa.commons.response.UpdateResponse;
 import it.unibo.arces.wot.sepa.engine.scheduling.InternalUpdateRequest;
 import it.unibo.arces.wot.sepa.engine.scheduling.ScheduledRequest;
+import it.unibo.arces.wot.sepa.engine.scheduling.Scheduler;
 
 class UpdateProcessingThread extends Thread {
 	private static final Logger logger = LogManager.getLogger();
 	
 	private final Processor processor;
+	private final Scheduler scheduler;
+	private final UpdateProcessor endpoint;
 	
 	public UpdateProcessingThread(Processor processor) {
-		this.processor = processor;
 		setName("SEPA-Update-Processor");
+		
+		this.processor = processor;
+		this.scheduler = processor.getScheduler();
+		this.endpoint = processor.getUpdateProcessor();
 	}
 
 	public void run() {
 		while (processor.isRunning()) {
 			ScheduledRequest request;
 			try {
-				request = processor.getScheduler().waitUpdateRequest();
+				request = scheduler.waitUpdateRequest();
 			} catch (InterruptedException e) {
 				return;
 			}
@@ -33,16 +39,16 @@ class UpdateProcessingThread extends Thread {
 			InternalUpdateRequest update = (InternalUpdateRequest)request.getRequest();
 			
 			// Notify update (not reliable)
-			if (!processor.isUpdateReilable()) processor.getScheduler().addResponse(request.getToken(),new UpdateResponse("Processing: "+update));
+			if (!processor.isUpdateReilable()) scheduler.addResponse(request.getToken(),new UpdateResponse("Processing: "+update));
 						
 			// PRE-processing update request
 			InternalUpdateRequest preRequest;
 			try {
-				preRequest = processor.getUpdateProcessor().preProcess(update);
+				preRequest = endpoint.preProcess(update);
 			} catch (SEPAProcessingException e2) {
-				logger.error("Pre processing aborted: "+e2.getMessage());
-				ErrorResponse errorResponse = new ErrorResponse(500, "PreProcessing failed: "+update, e2.getMessage());
-				processor.getScheduler().addResponse(request.getToken(),errorResponse);
+				logger.error("Update pre-processing failed: "+update+" exception: "+e2.getMessage());
+				ErrorResponse errorResponse = new ErrorResponse(502, "Endpoint update pre-processing failed: "+update, e2.getMessage());
+				scheduler.addResponse(request.getToken(),errorResponse);
 				continue;
 			}
 			
@@ -52,29 +58,29 @@ class UpdateProcessingThread extends Thread {
 			} catch (SEPAProcessingException e) {
 				logger.error("PreUpdateProcessing failed: "+e.getMessage());
 				ErrorResponse errorResponse = new ErrorResponse(500, "PreUpdateProcessing failed: "+update, e.getMessage());
-				processor.getScheduler().addResponse(request.getToken(),errorResponse);
+				scheduler.addResponse(request.getToken(),errorResponse);
 				continue;
 			}
 			
 			// Processing UPDATE
 			Response ret;
 			try {
-				ret = processor.getUpdateProcessor().process(preRequest);
+				ret = endpoint.process(preRequest);
 			} catch (InterruptedException e1) {
 				logger.error("Processing failed: "+e1.getMessage());
-				ErrorResponse errorResponse = new ErrorResponse(500, "Processing failed: "+update, e1.getMessage());
-				processor.getScheduler().addResponse(request.getToken(),errorResponse);
+				ErrorResponse errorResponse = new ErrorResponse(502, "Endpoint update processing failed: "+update, e1.getMessage());
+				scheduler.addResponse(request.getToken(),errorResponse);
 				continue;
 			}
 
 			// Notify update result
-			if (processor.isUpdateReilable()) processor.getScheduler().addResponse(request.getToken(),ret);
+			if (processor.isUpdateReilable()) scheduler.addResponse(request.getToken(),ret);
 
 			// Subscription processing (post update)
 			try {
 				processor.postUpdateProcessing(ret);
 			} catch (SEPAProcessingException e) {
-				logger.warn("Post processing failed: "+e.getMessage());
+				logger.warn("Post update processing failed: "+e.getMessage());
 				continue;
 			}
 		}
