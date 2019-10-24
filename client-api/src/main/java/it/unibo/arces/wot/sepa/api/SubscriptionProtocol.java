1cf8763f8a9e57fc07b4a8818146c01f34dc6f71
Luca Roffia <lroffia@gmail.com>
2019-10-24 16:16:41 +0200

    feat: add license statement to each file

--- client-api/src/main/java/it/unibo/arces/wot/sepa/api/SubscriptionProtocol.java
+++ client-api/src/main/java/it/unibo/arces/wot/sepa/api/SubscriptionProtocol.java
@@ -1,3 +1,21 @@
+/* The interface to be implemented by a subscription protocol implementation 
+ * 
+ * Author: Luca Roffia (luca.roffia@unibo.it)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package it.unibo.arces.wot.sepa.api;
 
 import java.io.Closeable;
@@ -13,8 +31,6 @@ public interface SubscriptionProtocol extends Closeable {
 	public void setHandler(ISubscriptionHandler handler);
 	
 	public void enableSecurity(SEPASecurityManager sm) throws SEPASecurityException;
-
-//	public void close() throws IOException;
 
 	public void subscribe(SubscribeRequest request) throws SEPAProtocolException;
 
