541c9f86358df304eda8a635d6f30ee62a608c18
Luca Roffia <lroffia@gmail.com>
2019-12-04 10:51:57 +0100

    Added comments for now() function

--- engine/src/main/java/it/unibo/arces/wot/sepa/engine/scheduling/InternalUQRequest.java
+++ engine/src/main/java/it/unibo/arces/wot/sepa/engine/scheduling/InternalUQRequest.java
@@ -60,10 +60,38 @@ public abstract class InternalUQRequest extends InternalRequest {
 		return sparql.equals(((InternalUQRequest)obj).sparql);
 	}
 	
-	// Override the SPARQL now() function to return the UTC time
+	/* Override the SPARQL now() function to return the current UTC time as xsd:dateTimeStamp
+	 
+	 https://www.w3.org/TR/sparql11-query/#func-now
+	 
+	 17.4.5.1 now
+ 		
+ 		xsd:dateTime  NOW ()
+
+		Returns an XSD dateTime value for the current query execution. 
+		All calls to this function in any one query execution must return the same value. 
+		The exact moment returned is not specified.
+
+		now()	"2011-01-10T14:45:13.815-05:00"^^xsd:dateTime
+		
+	https://www.w3.org/TR/xmlschema11-2/#dateTime
+	
+	3.3.7 dateTime
+
+		dateTime represents instants of time, optionally marked with a particular time zone offset.  
+		
+		Values representing the same instant but having different time zone offsets are equal but not identical.
+
+	https://www.w3.org/TR/xmlschema11-2/#dateTimeStamp
+
+	3.4.28 dateTimeStamp
+		[Definition:]   The dateTimeStamp datatype is ·derived· from dateTime by giving the value required to its explicitTimezone facet. 
+		The result is that all values of dateTimeStamp are required to have explicit time zone offsets and the datatype is totally ordered.
+
+	 * */
 	private String evaluateFunctions(String sparql) {
 		OffsetDateTime utc = OffsetDateTime.now(ZoneOffset.UTC);
-		String timestamp = "'" + utc.toString() + "'^^xsd:dateTime";
+		String timestamp = "'" + utc.toString() + "'^^xsd:dateTimeStamp";
 		
 		String temp = sparql.replace("now()", timestamp);
 		
