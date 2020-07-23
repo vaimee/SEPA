9129de7629f4a7b2ab8f3ea26395dc06bd194cd1
Luca Roffia <lroffia@gmail.com>
2020-07-23 12:02:21 +0200

    fix: Default timeout 1000 ms (was 5000 ms)

--- client-api/src/test/java/it/unibo/arces/wot/sepa/ConfigurationProvider.java
+++ client-api/src/test/java/it/unibo/arces/wot/sepa/ConfigurationProvider.java
@@ -61,7 +61,7 @@ public class ConfigurationProvider {
 			TIMEOUT = appProfile.getExtendedData().get("timeout").getAsLong();
 		}
 		else
-			TIMEOUT = 5000;
+			TIMEOUT = 1000;
 		
 		if (appProfile.getExtendedData().has("nretry")) {
 			NRETRY = appProfile.getExtendedData().get("nretry").getAsLong();
