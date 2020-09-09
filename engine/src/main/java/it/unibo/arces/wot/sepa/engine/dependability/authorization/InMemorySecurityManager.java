a01cd77249678ca8acaf5951d1ffe0d5e8f08f65
Luca Roffia <lroffia@gmail.com>
2020-09-09 16:05:44 +0200

    feat: Implemented KeyCloak integration

--- engine/src/main/java/it/unibo/arces/wot/sepa/engine/dependability/authorization/InMemorySecurityManager.java
+++ engine/src/main/java/it/unibo/arces/wot/sepa/engine/dependability/authorization/InMemorySecurityManager.java
@@ -1,30 +1,12 @@
-/* The class implements a in-memory (volatile) authorization
- * 
- * Author: Luca Roffia (luca.roffia@unibo.it)
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 package it.unibo.arces.wot.sepa.engine.dependability.authorization;
 
 import java.text.ParseException;
 import java.util.Date;
 import java.util.HashMap;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import javax.net.ssl.SSLContext;
 
+import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
 
@@ -34,8 +16,14 @@ import it.unibo.arces.wot.sepa.engine.dependability.authorization.identities.App
 import it.unibo.arces.wot.sepa.engine.dependability.authorization.identities.DeviceIdentity;
 import it.unibo.arces.wot.sepa.engine.dependability.authorization.identities.DigitalIdentity;
 
-public class InMemoryAuthorization implements IAuthorization {
-	private static final Logger logger = LogManager.getLogger();
+public class InMemorySecurityManager extends SecurityManager {
+	public InMemorySecurityManager(SSLContext ssl,RSAKey key)
+			throws SEPASecurityException {
+		super(ssl, key,true);
+		
+		// TODO: WARNING!!! JUST FOR TESTING
+		addAuthorizedIdentity(new ApplicationIdentity("SEPATest",new Credentials("SEPATest","SEPATest")));
+	}
 	
 	class AuthorizedIdentity {
 		DigitalIdentity identity = null;
@@ -138,20 +126,12 @@ public class InMemoryAuthorization implements IAuthorization {
 	private final HashMap<String,AuthorizedIdentity> identities = new HashMap<String,AuthorizedIdentity>();
 
 	private String issuer = "https://localhost:8443/oauth/token";
-//	private String httpsAudience = "https://wot.arces.unibo.it:8443/sparql";
-//	private String wssAudience = "wss://wot.arces.unibo.it:9443/sparql";
-//	private String subject = "SEPATest";
 
 	private long deviceExpiringTime = 3600;
 	private long applicationExpiringTime = 43200;
 	private long userExpiringTime = 300;
 	private long defaultExpiringTime = 5;
 	private long SEPATestExpiringTime = 5;
-	
-	public InMemoryAuthorization() {
-		// TODO: WARNING!!! JUST FOR TESTING
-		addAuthorizedIdentity(new ApplicationIdentity("SEPATest",new Credentials("SEPATest","SEPATest")));
-	}
 	
 	@Override
 	public boolean isAuthorized(String uid) throws SEPASecurityException {
@@ -214,7 +194,7 @@ public class InMemoryAuthorization implements IAuthorization {
 	
 	// Client claims
 	@Override
-	public boolean containsToken(String id) {
+	public boolean containsJwt(String id) {
 		logger.debug("containsToken "+id);
 		
 		if (identities.containsKey(id)) return identities.get(id).containsToken();
@@ -232,14 +212,14 @@ public class InMemoryAuthorization implements IAuthorization {
 	}
 	
 	@Override
-	public void addToken(String id, SignedJWT jwt) throws SEPASecurityException {
+	public void addJwt(String id, SignedJWT jwt) throws SEPASecurityException {
 		logger.debug("addToken "+id+" "+jwt.serialize());
 		
 		if (identities.containsKey(id)) identities.get(id).addToken(jwt);
 	}
 	
 	@Override
-	public void removeToken(String id) throws SEPASecurityException {
+	public void removeJwt(String id) throws SEPASecurityException {
 		logger.debug("removeToken "+id);
 		
 		if (identities.containsKey(id)) identities.get(id).removeToken();
@@ -263,7 +243,7 @@ public class InMemoryAuthorization implements IAuthorization {
 	}
 	
 	@Override
-	public SignedJWT getToken(String id) {
+	public SignedJWT getJwt(String id) {
 		logger.debug("getToken "+id);
 		
 		if (identities.containsKey(id)) return identities.get(id).getToken();
---
+++ engine/src/main/java/it/unibo/arces/wot/sepa/engine/dependability/authorization/KeyCloakSecurityManager.java
@@ -0,0 +1,299 @@
+package it.unibo.arces.wot.sepa.engine.dependability.authorization;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.net.ssl.SSLContext;
+
+import org.apache.http.HttpStatus;
+
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+
+import it.unibo.arces.wot.sepa.commons.exceptions.SEPASecurityException;
+import it.unibo.arces.wot.sepa.commons.response.ErrorResponse;
+import it.unibo.arces.wot.sepa.commons.response.Response;
+import it.unibo.arces.wot.sepa.commons.security.ClientAuthorization;
+import it.unibo.arces.wot.sepa.commons.security.Credentials;
+import it.unibo.arces.wot.sepa.engine.dependability.authorization.identities.DigitalIdentity;
+
+public class KeyCloakSecurityManager extends SecurityManager {
+
+	private SyncLdap ldap;
+	private VirtuosoIsql isql;
+
+	public KeyCloakSecurityManager(SSLContext ssl, RSAKey key,LdapProperties prop, IsqlProperties isqlprop)
+			throws SEPASecurityException {
+		super(ssl, key, false);
+
+		ldap = new SyncLdap(prop);
+
+		isql = new VirtuosoIsql(isqlprop,ldap.getEndpointUsersPassword());
+
+		new UsersSync(ldap, isql);
+		
+		logger.debug("EndpointUsersPassword: "+ldap.getEndpointUsersPassword());
+	}
+	
+	@Override
+	public synchronized Response register(String uid) {
+		return new ErrorResponse(HttpStatus.SC_UNAUTHORIZED, "not supported", "Implemented by KeyCloak");
+	}
+	
+	@Override
+	public synchronized Response getToken(String encodedCredentials) {
+		return new ErrorResponse(HttpStatus.SC_UNAUTHORIZED, "not supported", "Implemented by KeyCloak");
+	}
+
+	/** Requesting Party Token 
+	 * 
+	 * If you want to validate these tokens without a call to the remote introspection endpoint, you can decode the RPT and query for its validity locally. 
+	 * Once you decode the token, you can also use the permissions within the token to enforce authorization decisions.
+	 * 
+	 * This is essentially what the policy enforcers do. Be sure to:
+	 * 1) Validate the signature of the RPT (based on the realm’s public key)
+	 * 2) Query for token validity based on its exp, iat, and aud claims
+	 * 
+	 * The claim "preferred_username" is used to identify the user
+	 * */
+	@Override
+	public synchronized ClientAuthorization validateToken(String accessToken) {
+		logger.debug("VALIDATE TOKEN");
+
+		// Parse token
+		SignedJWT signedJWT = null;
+		try {
+			signedJWT = SignedJWT.parse(accessToken);
+		} catch (ParseException e) {
+			logger.error(e.getMessage());
+			return new ClientAuthorization("invalid_request", "ParseException: " + e.getMessage());
+		}
+
+		// Verify token
+		try {
+			if (!signedJWT.verify(verifier)) {
+				logger.error("Signed JWT not verified");
+				return new ClientAuthorization("invalid_grant", "Signed JWT not verified");
+			}
+
+		} catch (JOSEException e) {
+			logger.error(e.getMessage());
+			return new ClientAuthorization("invalid_grant", "JOSEException: " + e.getMessage());
+		}
+		
+
+		String uid;
+		// Process token (validate)
+		JWTClaimsSet claimsSet = null;
+		try {
+			claimsSet = signedJWT.getJWTClaimsSet();
+			logger.debug(claimsSet);
+			// Get client credentials for accessing the SPARQL endpoint
+			uid = claimsSet.getStringClaim("preferred_username");
+			logger.debug("Subject: "+claimsSet.getSubject());
+			logger.debug("Issuer: "+claimsSet.getIssuer());
+			logger.debug("Username: "+uid);
+		} catch (ParseException e) {
+			logger.error(e.getMessage());
+			return new ClientAuthorization("invalid_grant", "ParseException. " + e.getMessage());
+		}
+
+
+		// Check token expiration (an "invalid_grant" error is raised if the token is
+		// expired)
+		Date now = new Date();
+		long nowUnixSeconds = (now.getTime() / 1000) * 1000;
+		Date expiring = claimsSet.getExpirationTime();
+		Date notBefore = claimsSet.getNotBeforeTime();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
+		if (expiring.getTime() - nowUnixSeconds < 0) {
+			logger.warn("Token is expired: " + sdf.format(claimsSet.getExpirationTime()) + " < "
+					+ sdf.format(new Date(nowUnixSeconds)));
+
+			return new ClientAuthorization("invalid_grant", "Token issued at " + sdf.format(claimsSet.getIssueTime())
+					+ " is expired: " + sdf.format(claimsSet.getExpirationTime()) + " < " + sdf.format(now));
+		}
+
+		if (notBefore != null && nowUnixSeconds < notBefore.getTime()) {
+			logger.warn("Token can not be used before: " + claimsSet.getNotBeforeTime());
+			return new ClientAuthorization("invalid_grant",
+					"Token can not be used before: " + claimsSet.getNotBeforeTime());
+		}
+		
+		Credentials cred = null;
+		try {
+			cred = getEndpointCredentials(uid);
+			logger.debug("Endpoint credentials: "+cred);
+		} catch (SEPASecurityException e) {
+			logger.error("Failed to retrieve credentials (" + uid + ")");
+			return new ClientAuthorization("invalid_grant", "Failed to get credentials (" + uid + ")");
+		}
+
+		return new ClientAuthorization(cred);
+	}
+
+	@Override
+	public void addAuthorizedIdentity(DigitalIdentity identity) throws SEPASecurityException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeAuthorizedIdentity(String uid) throws SEPASecurityException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public DigitalIdentity getIdentity(String uid) throws SEPASecurityException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAuthorized(String identity) throws SEPASecurityException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isForTesting(String identity) throws SEPASecurityException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean storeCredentials(DigitalIdentity identity, String secret) throws SEPASecurityException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void removeCredentials(DigitalIdentity identity) throws SEPASecurityException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean containsCredentials(String uid) throws SEPASecurityException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean checkCredentials(String uid, String secret) throws SEPASecurityException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Credentials getEndpointCredentials(String uid) throws SEPASecurityException {
+		return new Credentials(uid, ldap.getEndpointUsersPassword());
+	}
+
+	@Override
+	public void addJwt(String id, SignedJWT claims) throws SEPASecurityException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean containsJwt(String id) throws SEPASecurityException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public SignedJWT getJwt(String uid) throws SEPASecurityException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void removeJwt(String id) throws SEPASecurityException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Date getTokenExpiringDate(String id) throws SEPASecurityException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getTokenExpiringPeriod(String id) throws SEPASecurityException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setTokenExpiringPeriod(String id, long period) throws SEPASecurityException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setDeviceExpiringPeriod(long period) throws SEPASecurityException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public long getDeviceExpiringPeriod() throws SEPASecurityException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setApplicationExpiringPeriod(long period) throws SEPASecurityException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public long getApplicationExpiringPeriod() throws SEPASecurityException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setUserExpiringPeriod(long period) throws SEPASecurityException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public long getUserExpiringPeriod() throws SEPASecurityException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setDefaultExpiringPeriod(long period) throws SEPASecurityException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public long getDefaultExpiringPeriod() throws SEPASecurityException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getIssuer() throws SEPASecurityException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setIssuer(String is) throws SEPASecurityException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
