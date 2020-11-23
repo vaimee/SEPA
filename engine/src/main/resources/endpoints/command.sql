1040eb22afe7af8aa3f961b25dadfb907c311403
Luca Roffia <lroffia@gmail.com>
2020-11-23 16:46:30 +0100

    Fixed securitymanager test on engine

--- engine/src/main/resources/endpoints/command.sql
+++
@@ -1,5 +0,0 @@
-DB.DBA.USER_CREATE ('sepatest', 'virtuoso');
-DB.DBA.RDF_DEFAULT_USER_PERMS_SET ('nobody', 0);
-DB.DBA.RDF_DEFAULT_USER_PERMS_SET ('sepatest', 0);
-GRANT SPARQL_UPDATE TO "sepatest";
-DB.DBA.RDF_GRAPH_USER_PERMS_SET ('http://sepatest', 'sepatest', 3);
