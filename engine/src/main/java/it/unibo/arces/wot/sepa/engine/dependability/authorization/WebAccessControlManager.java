0d8390aabf25f2cb8e3dc8e22e33d628445b1ef8
Luca Roffia <lroffia@gmail.com>
2021-06-29 17:55:02 +0200

    Basic skeleton of WAC manager

--- engine/src/main/java/it/unibo/arces/wot/sepa/engine/dependability/authorization/WebAccessControlManager.java
+++ engine/src/main/java/it/unibo/arces/wot/sepa/engine/dependability/authorization/WebAccessControlManager.java
@@ -1,5 +1,32 @@
 package it.unibo.arces.wot.sepa.engine.dependability.authorization;
 
+import org.apache.http.impl.client.CloseableHttpClient;
+import com.google.gson.JsonObject;
+import com.nimbusds.jwt.SignedJWT;
+
+import it.unibo.arces.wot.sepa.engine.scheduling.JenaSparqlParsing;
+
 public class WebAccessControlManager {
 	//TODO: implement WAC
+	/** The http client. */
+	protected CloseableHttpClient httpClient;
+	
+	// Parse token
+	SignedJWT signedJWT = null;
+	// see validateToken method in class KeyCloakSecurityManager
+	
+	//JSON
+	private JsonObject results;
+	
+	// SPARQL
+	// https://jena.apache.org/
+	JenaSparqlParsing sparqlParser;
+	
+	void validateToken() {
+		
+	}
+	
+	void accessControl() {
+		
+	}
 }
