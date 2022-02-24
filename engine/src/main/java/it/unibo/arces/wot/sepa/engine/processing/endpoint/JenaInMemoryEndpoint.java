7deff2cd57ab5b04184b34a5704ac7c51ccb58a6
Luca Roffia <lroffia@gmail.com>
2022-02-24 11:20:02 +0100

    Jena in memory using create instead of createtcnMem for dataset

--- engine/src/main/java/it/unibo/arces/wot/sepa/engine/processing/endpoint/JenaInMemoryEndpoint.java
+++ engine/src/main/java/it/unibo/arces/wot/sepa/engine/processing/endpoint/JenaInMemoryEndpoint.java
@@ -40,20 +40,44 @@ import it.unibo.arces.wot.sepa.commons.response.QueryResponse;
 import it.unibo.arces.wot.sepa.commons.response.Response;
 import it.unibo.arces.wot.sepa.commons.response.UpdateResponse;
 
-public class JenaInMemoryEndpoint implements SPARQLEndpoint{
+/**
+ * 
+Special Graph Names
+URI	Meaning
+urn:x-arq:UnionGraph	The RDF merge of all the named graphs in the datasets of the query.
+urn:x-arq:DefaultGraph	The default graph of the dataset, used when the default graph of the query is the union graph.
+
+Note that setting tdb:unionDefaultGraph does not affect the default graph or default model obtained with dataset.getDefaultModel().
+
+The RDF merge of all named graph can be accessed as the named graph urn:x-arq:UnionGraph using Dataset.getNamedModel("urn:x-arq:UnionGraph") .
+ 
+An RDF Dataset is a collection of one, unnamed, default graph and zero, or more named graphs. 
+In a SPARQL query, a query pattern is matched against the default graph unless the GRAPH keyword is applied to a pattern.
+
+Transactions are part of the interface to RDF Datasets. There is a default implementation, based on MRSW locking (multiple-reader or single-writer) that can be used with any mixed set of components. Certain storage sub-systems provide better concurrency with MR+SW (multiple-read and single writer).
+
+Dataset		Facilities			Creation
+TxnMem		MR+SW				DatasetFactory.createTxnMem
+TDB			MR+SW, persistent	TDBFactory.create
+TDB2		MR+SW, persistent	TDB2Factory.create
+General		MRSW				DatasetFactory.create
+
+The general dataset can have any graphs added to it (e.g. inference graphs).
+ * */
+public class JenaInMemoryEndpoint implements SPARQLEndpoint {
 	protected static final Logger logger = LogManager.getLogger();
-	
-	static final Dataset dataset = DatasetFactory.createTxnMem();
-	
+
+	static final Dataset dataset = DatasetFactory.create(); // DatasetFactory.createTxnMem();
+
 	@Override
 	public Response query(QueryRequest req) {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		RDFConnection conn = RDFConnectionFactory.connect(dataset);
-		Txn.executeRead(conn, ()-> {
+		Txn.executeRead(conn, () -> {
 			ResultSet rs = conn.query(QueryFactory.create(req.getSPARQL())).execSelect();
 			ResultSetFormatter.outputAsJSON(out, rs);
 		});
-	 
+
 		try {
 			return new QueryResponse(out.toString(StandardCharsets.UTF_8.name()));
 		} catch (UnsupportedEncodingException e) {
@@ -64,7 +88,7 @@ public class JenaInMemoryEndpoint implements SPARQLEndpoint{
 	@Override
 	public Response update(UpdateRequest req) {
 		RDFConnection conn = RDFConnectionFactory.connect(dataset);
-		Txn.executeWrite(conn, ()-> {
+		Txn.executeWrite(conn, () -> {
 			conn.update(req.getSPARQL());
 		});
 		return new UpdateResponse("Jena-in-memory-update");
