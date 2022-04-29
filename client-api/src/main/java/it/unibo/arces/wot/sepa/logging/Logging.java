8794d6b2bd68040237819164a7d7e6c39c504e44
Luca Roffia <lroffia@gmail.com>
2022-04-29 08:55:11 +0200

    fixed bug on logging

--- client-api/src/main/java/it/unibo/arces/wot/sepa/logging/Logging.java
+++ client-api/src/main/java/it/unibo/arces/wot/sepa/logging/Logging.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import java.util.Iterator;
 import java.util.TimeZone;
 
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.LoggerContext;
@@ -15,35 +16,37 @@ import org.apache.logging.log4j.core.config.LoggerConfig;
 
 public class Logging {
 	public static final Logger logger = LogManager.getLogger();
-	
-	public static org.apache.logging.log4j.Level getLevel(String level) {
-		return (org.apache.logging.log4j.Level.getLevel(level) == null ? org.apache.logging.log4j.Level.OFF
-				: org.apache.logging.log4j.Level.getLevel(level));
+
+	public static Level getLevel(String level) {
+		return (Level.getLevel(level) == null ? Level.DEBUG : Level.getLevel(level));
 	}
 
-	// Logging file name
-	static {
-		// Logging
+	public static void init() {
 		TimeZone tz = TimeZone.getTimeZone("UTC");
 		DateFormat df = new SimpleDateFormat("yyyyMMdd_HH_mm_ss"); // Quoted "Z" to indicate GMT, no timezone offset
 		df.setTimeZone(tz);
 		String nowAsISO = df.format(new Date());
 		System.setProperty("logFilename", nowAsISO);
-		org.apache.logging.log4j.core.LoggerContext ctx = (org.apache.logging.log4j.core.LoggerContext) LogManager
-				.getContext(false);
+		LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
 		ctx.reconfigure();
-	}
-	
-	public static void printLog4jConfiguration() {
-		System.out.println(">>> Logging <<<");
-		System.out.println("Level: " + Logging.logger.getLevel().toString());
-		final LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
+
+		System.out.println("");
+		System.out.println("|------------ Logging ------------");
+		
 		final Configuration config = ctx.getConfiguration();
 		LoggerConfig rootLoggerConfig = config.getLoggers().get("");
 		Iterator<AppenderRef> it = rootLoggerConfig.getAppenderRefs().iterator();
 		while (it.hasNext()) {
 			AppenderRef ref = it.next();
-			System.out.println("Appender: <" + ref.getRef() + "> Level: " + ref.getLevel());
+			System.out.println("| Appender: <" + ref.getRef() + "> Level: " + ref.getLevel());
+			if (rootLoggerConfig.getAppenderRefs().size() == 1 && ref.getRef().equals("OUT") && ref.getLevel() == null) {
+				LoggerConfig loggerConfig = config.getLoggerConfig(LogManager.ROOT_LOGGER_NAME); 
+				loggerConfig.setLevel(Level.ERROR);
+				ctx.updateLoggers();
+			}
 		}
+		
+		System.out.println("| Logger level: " + logger.getLevel().toString());
+		System.out.println("|---------------------------------");
 	}
 }
