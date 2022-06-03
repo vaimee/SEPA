90d2c9365b185e221239f7e2b2f76c32887aba5f
trivo78 <trivo78@gmail.com>
2022-06-03 07:14:23 +0200

    added parse of user information directly from InternalRequest class

--- engine/src/main/java/it/unibo/arces/wot/sepa/engine/acl/SEPAUserInfo.java
+++ engine/src/main/java/it/unibo/arces/wot/sepa/engine/acl/SEPAUserInfo.java
@@ -5,6 +5,10 @@
  */
 package it.unibo.arces.wot.sepa.engine.acl;
 
+import it.unibo.arces.wot.sepa.commons.exceptions.SEPASecurityException;
+import it.unibo.arces.wot.sepa.engine.bean.EngineBeans;
+import it.unibo.arces.wot.sepa.engine.scheduling.InternalRequest;
+
 /**
  *
  * @author Lorenzo
@@ -12,11 +16,40 @@ package it.unibo.arces.wot.sepa.engine.acl;
 public class SEPAUserInfo {
     public final String userName;
     
-    public static SEPAUserInfo newInstance(String userName) {
-        return new SEPAUserInfo(userName);
+    private static boolean  hasACLSupport;
+    private static boolean  hasInit;
+    
+    public static SEPAUserInfo newInstance(InternalRequest req) throws SEPASecurityException {
+        if (hasInit == false) {
+            hasACLSupport = EngineBeans.isAclEnabled();
+            hasInit = true;
+        }
+        
+        if (hasACLSupport == false)
+            return null;
+        
+        SEPAUserInfo  ret = null;
+        if (    req != null                                                 && 
+                req.getClientAuthorization() != null                        &&
+                req.getClientAuthorization().getCredentials()  != null
+
+        ) {
+            ret  = new SEPAUserInfo(req.getClientAuthorization().getCredentials().user());
+        }
+        
+        if (ret == null || ret.validate() == false ) {
+            throw new SEPASecurityException("Unable to find user information for ACL check");
+        }
+        
+        return ret;
+        
     }
     
     private SEPAUserInfo(String n) {
         userName = n;
+    }
+    
+    public boolean validate() {
+        return userName != null;
     }
 } 
