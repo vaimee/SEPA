c21f5c4728cc08f3cea4dc9bb86689243070c65e
trivo78 <trivo78@gmail.com>
2022-06-16 06:07:06 +0200

    moved default file names to public static consts

--- engine/src/main/java/it/unibo/arces/wot/sepa/engine/core/Engine.java
+++ engine/src/main/java/it/unibo/arces/wot/sepa/engine/core/Engine.java
@@ -74,9 +74,11 @@ public class Engine implements EngineMBean {
 	private HttpsGate httpsGate = null;
 	private int wsShutdownTimeout = 5000;
 
+        public static final String defaultEngineJpar = "engine.jpar";
+        public static final String defaultEndpointJpar = "endpoint.jpar";
 	// Properties files
-	private String engineJpar = "engine.jpar";
-	private String endpointJpar = "endpoint.jpar";
+	private String engineJpar = defaultEngineJpar;
+	private String endpointJpar = defaultEndpointJpar;
 
 	// Secure option
 	private Optional<Boolean> secure = Optional.empty();
