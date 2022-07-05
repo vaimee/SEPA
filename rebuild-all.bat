9e02f9d56af229068b476b7389e92589518954dd
trivo78 <trivo78@gmail.com>
2022-07-05 04:53:33 +0200

    moved some sample jpar files outside build directory

---
+++ rebuild-all.bat
@@ -0,0 +1,7 @@
+@rem Licensed under the terms of http://www.apache.org/licenses/LICENSE-2.0
+
+SET PATH=C:\apache-maven-3.8.4\bin;C:\Program Files\Java\jdk-11.0.12\bin;C:\Program Files\Python\Python39\Scripts\;C:\Program Files\Python\Python39\;C:\windows\system32;C:\windows;C:\windows\System32\Wbem;C:\windows\System32\WindowsPowerShell\v1.0\;C:\windows\System32\OpenSSH\;C:\Program Files (x86)\NVIDIA Corporation\PhysX\Common;C:\Program Files\Microsoft VS Code\bin;C:\Program Files\Git\cmd;C:\Program Files\dotnet\;C:\Program Files\Microsoft SQL Server\130\Tools\Binn\;C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\170\Tools\Binn\;C:\Program Files (x86)\dotnet\;C:\Users\Lorenzo\AppData\Local\Microsoft\WindowsApps;C:\Users\Lorenzo\.dotnet\tools;C:\Users\Lorenzo\AppData\Local\Programs\MiKTeX\miktex\bin\x64\;C:\MiKTeX\miktex\bin\x64\;C:\Users\Lorenzo\AppData\Local\GitHubDesktop\bin 
+
+SET JAVA_HOME=C:\Program Files\Java\jdk-11.0.12
+
+mvn install  -DskipTests
