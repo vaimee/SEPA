27c02c2c037670b6da3d58a930ab0fd649a6056a
Luca Roffia <lroffia@gmail.com>
2024-11-27 12:36:21 +0100

    build: config: new Dockerfile for JMX

--- client-api/src/main/java/it/unibo/arces/wot/sepa/logging/Logging.java
+++ client-api/src/main/java/it/unibo/arces/wot/sepa/logging/Logging.java
@@ -9,10 +9,11 @@ import java.util.TimeZone;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.logging.log4j.core.config.AppenderRef;
 import org.apache.logging.log4j.core.config.Configuration;
 import org.apache.logging.log4j.core.config.LoggerConfig;
+import org.apache.logging.log4j.core.LoggerContext;
+
 
 public class Logging {
 	public static final Logger logger = LogManager.getLogger();
