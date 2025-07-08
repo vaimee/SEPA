16e603d6a527bf684a415d355b816f63ee95bb4a
Luca Roffia <lroffia@gmail.com>
2025-07-08 18:20:14 +0200

    fix: directory structure

--- dash2/src/main/java/com/vaimee/sepa/tools/dashboard/utils/LoginListener.java
+++
@@ -1,9 +0,0 @@
-package com.vaimee.sepa.tools.dashboard.utils;
-
-import com.vaimee.sepa.api.commons.response.ErrorResponse;
-
-public interface LoginListener {
-	void onLogin(String id);//, String secret,boolean remember);
-	void onLoginError(ErrorResponse err);
-	void onLoginClose();
-}
