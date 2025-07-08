3c3ff54abf976fe0bd1fd57aa6acff57c0bf06ac
Luca Roffia <lroffia@gmail.com>
2025-07-08 18:16:12 +0200

    [fix] directory structure

--- chat2/src/main/java/com/vaimee/sepa/apps/chat/IMessageHandler.java
+++
@@ -1,7 +0,0 @@
-package com.vaimee.sepa.apps.chat;
-
-public interface IMessageHandler {
-	public void onMessageReceived(String userUri,String fromUri,String messageUri,String user,String message,String time);
-	public void onMessageRemoved(String userUri, String toUri,String messageUri, String user, String message,String time);
-	public void onMessageSent(String userUri, String toUri,String messageUri,String time);	
-}
