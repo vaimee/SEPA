97afa96484193dc5458e2134b1831f7e8090c400
Luca Roffia <lroffia@gmail.com>
2025-07-20 17:36:55 +0200

    cleaning

--- engine/src/main/java/com/vaimee/sepa/engine/extensions/EchoHandler.java
+++ engine/src/main/java/com/vaimee/sepa/engine/extensions/EchoHandler.java
@@ -1,5 +1,6 @@
 package com.vaimee.sepa.engine.extensions;
 
+import com.vaimee.sepa.engine.bean.SEPABeans;
 import com.vaimee.sepa.engine.gates.http.HttpUtilities;
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
@@ -8,8 +9,12 @@ import org.apache.http.entity.ContentType;
 import org.apache.http.nio.entity.NStringEntity;
 import org.apache.http.protocol.HttpContext;
 
-public class EchoHandler extends Extension{
-    @Override
+public class EchoHandler extends Extension implements EchoHandlerMBean{
+
+    public EchoHandler() {
+        super();
+        SEPABeans.registerMBean("SEPA:type=" + this.getClass().getSimpleName(), this);
+    }
     public void handleInternal(HttpRequest request, HttpResponse response, HttpContext context, String body) {
         response.setStatusCode(HttpStatus.SC_OK);
         NStringEntity entity = new NStringEntity(
