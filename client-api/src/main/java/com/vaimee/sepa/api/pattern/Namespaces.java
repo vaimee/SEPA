863189b86b3b36c05d7425fee3ede803b2e8fbd9
Luca Roffia <lroffia@gmail.com>
2025-07-29 22:40:52 +0200

    ci: fix warnings and Maven for deploy on GitHub

--- client-api/src/main/java/com/vaimee/sepa/api/pattern/Namespaces.java
+++ client-api/src/main/java/com/vaimee/sepa/api/pattern/Namespaces.java
@@ -4,7 +4,7 @@ import com.vaimee.sepa.api.commons.exceptions.SEPABindingsException;
 import com.vaimee.sepa.api.commons.sparql.Bindings;
 import com.vaimee.sepa.logging.Logging;
 
-import org.apache.commons.lang3.StringEscapeUtils;
+import org.apache.commons.text.StringEscapeUtils;
 
 import org.apache.jena.atlas.io.IndentedLineBuffer;
 import org.apache.jena.atlas.io.IndentedWriter;
