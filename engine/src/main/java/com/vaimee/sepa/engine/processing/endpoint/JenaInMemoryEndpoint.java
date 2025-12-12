4e159a4924b3c5ddf2a734e6b200fed5465ac21d
Luca Roffia <lroffia@gmail.com>
2025-12-12 16:59:02 +0100

    feat geosparql working with select, ask and construct

--- engine/src/main/java/com/vaimee/sepa/engine/processing/endpoint/JenaInMemoryEndpoint.java
+++ engine/src/main/java/com/vaimee/sepa/engine/processing/endpoint/JenaInMemoryEndpoint.java
@@ -18,28 +18,29 @@
 
 package com.vaimee.sepa.engine.processing.endpoint;
 
+import com.google.gson.*;
 import com.vaimee.sepa.api.commons.request.QueryRequest;
 import com.vaimee.sepa.api.commons.request.UpdateRequest;
 import com.vaimee.sepa.api.commons.response.*;
+import com.vaimee.sepa.api.commons.sparql.*;
 import com.vaimee.sepa.engine.bean.EngineBeans;
 import com.vaimee.sepa.engine.dependability.acl.SEPAUserInfo;
-import org.apache.jena.query.Dataset;
-import org.apache.jena.query.QueryFactory;
-import org.apache.jena.query.ResultSet;
-import org.apache.jena.query.ResultSetFormatter;
+import org.apache.jena.geosparql.configuration.GeoSPARQLConfig;
+import org.apache.jena.query.*;
+import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdfconnection.RDFConnection;
 import org.apache.jena.rdfconnection.RDFConnectionFactory;
 import org.apache.jena.sparql.core.Quad;
 import org.apache.jena.sparql.modify.UpdateResult;
 import org.apache.jena.system.Txn;
+import org.glassfish.grizzly.http.util.HttpStatus;
 
 import java.io.ByteArrayOutputStream;
+import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Special Graph Names
@@ -50,45 +51,109 @@ import java.util.TreeSet;
 
 public class JenaInMemoryEndpoint implements SPARQLEndpoint {
     private static Dataset dataset;
-    private static boolean hasInit;
 
-    public synchronized static void init() {
-        if (hasInit == false) {
-            dataset = JenaDatasetFactory.newInstance(EngineBeans.getFirstDatasetMode(), EngineBeans.getFirstDatasetPath(), true);
-            hasInit = true;
-        }
+    public JenaInMemoryEndpoint() {
+        dataset = JenaDatasetFactory.newInstance(EngineBeans.getFirstDatasetMode(), EngineBeans.getFirstDatasetPath(), true);
+        GeoSPARQLConfig.setupMemoryIndex();
     }
 
     @Override
     public Response query(QueryRequest req, SEPAUserInfo usr) {
-        init();
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        AtomicReference<Response> response = new AtomicReference<>();
 
-        try (final RDFConnection conn =
-                     (usr != null && usr.userName != null && usr.userName.trim().length() > 0) ?
-                             RDFConnectionFactory.connect(dataset, usr.userName) :
-                             RDFConnectionFactory.connect(dataset);
-        ) {
-
-
+        try {
+            final RDFConnection conn =
+                    (usr != null && usr.userName != null && usr.userName.trim().length() > 0) ?
+                            RDFConnectionFactory.connect(dataset, usr.userName) :
+                            RDFConnectionFactory.connect(dataset);
             Txn.executeRead(conn, () -> {
+                Query query = QueryFactory.create(req.getSPARQL());
                 //TODO: Consider all query types (ASK, CONSTRUCT, ...)
-                ResultSet rs = conn.query(QueryFactory.create(req.getSPARQL())).execSelect();
-                ResultSetFormatter.outputAsJSON(out, rs);
+                if (query.isSelectType()) {
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    ResultSet rs = conn.query(QueryFactory.create(req.getSPARQL())).execSelect();
+                    ResultSetFormatter.outputAsJSON(out, rs);
+                    try {
+                        response.set(new QueryResponse(out.toString(StandardCharsets.UTF_8.name())));
+                    } catch (UnsupportedEncodingException e) {
+                        response.set(new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR_500.getStatusCode(), "Failed to query Jena in memory endpoint",e.getMessage()));
+                    }
+                } else if (query.isAskType()) {
+                    boolean res = conn.queryAsk(query);
+                    JsonObject json = new JsonObject();
+                    json.add("boolean", new JsonPrimitive(res));
+                    json.add("head", new JsonObject());
+                    response.set(new QueryResponse(json.toString()));
+                } else if (query.isConstructType()) {
+                    Model res = conn.queryConstruct(query);
+                    response.set(new QueryResponse(modelToBindingsResults(res).toString()));
+                }
             });
 
-            try {
-                return new QueryResponse(out.toString(StandardCharsets.UTF_8.name()));
-            } catch (UnsupportedEncodingException e) {
-                return new ErrorResponse(500, "UnsupportedEncodingException", e.getMessage());
+            return response.get();
+        }
+        catch (Exception e) {
+            return new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR_500.getStatusCode(), "Failed to query Jena in memory endpoint",e.getMessage());
+        }
+    }
+
+    public BindingsResults modelToBindingsResults(Model res) {
+        StringWriter sw = new StringWriter();
+        res.write(sw, "RDF/JSON");
+
+        JsonObject json;
+        try {
+            json = new Gson().fromJson(sw.toString(),JsonObject.class);
+        } catch (JsonParseException e) {
+            json = null;
+        }
+
+        ArrayList<String> vars = new ArrayList<>();
+        vars.add("subject");
+        vars.add("predicate");
+        vars.add("object");
+
+        ArrayList<Bindings> bindings = new ArrayList<>();
+
+        assert json != null;
+
+        for (String subject : json.keySet()) {
+            RDFTerm s;
+            try{
+                s = new RDFTermURI(subject);
+            }catch (Exception e) {
+                s = new RDFTermBNode(subject);
+            }
+
+            JsonObject predicates = json.getAsJsonObject(subject);
+            for (String predicate : predicates.keySet()) {
+                JsonArray objects =  predicates.getAsJsonArray(predicate);
+                for (JsonElement obj : objects) {
+                    String type = obj.getAsJsonObject().getAsJsonPrimitive("type").getAsString();
+                    String value = obj.getAsJsonObject().getAsJsonPrimitive("value").getAsString();
+
+                    Bindings b = new Bindings();
+                    b.addBinding("subject",s);
+                    b.addBinding("predicate",new RDFTermURI(predicate));
+
+                    if(type.equals("uri")) b.addBinding("object",new RDFTermURI(value));
+                    else if(type.equals("bnode")) b.addBinding("object",new RDFTermBNode(value));
+                    else {
+                        String datatype = (obj.getAsJsonObject().has("datatype") ? obj.getAsJsonObject().getAsJsonPrimitive("datatype").getAsString() : null);
+                        String lang = (obj.getAsJsonObject().has("lang") ? obj.getAsJsonObject().getAsJsonPrimitive("lang").getAsString() : null);
+                        b.addBinding("object",new RDFTermLiteral(value,datatype,lang));
+                    }
+
+                    bindings.add(b);
+                }
             }
         }
+
+        return new BindingsResults(vars,bindings);
     }
 
     @Override
     public Response update(UpdateRequest req, SEPAUserInfo usr) {
-        init();
-
         try (final RDFConnection conn =
                      (usr != null && usr.userName != null && usr.userName.trim().length() > 0) ?
                              RDFConnectionFactory.connect(dataset, usr.userName) :
@@ -118,7 +183,7 @@ public class JenaInMemoryEndpoint implements SPARQLEndpoint {
 
             });
 
-            return new UpdateResponseWithAR(added,removed);
+            return new UpdateResponseWithAR(added, removed);
         }
     }
 
